822425497504682dc0c13a7743004d0f61738fa7
Econ Account <ouecon003@schooner2.oscer.ou.edu>
2023-02-13 20:16:03 -0600

    Update PS3

--- ProblemSets/PS3/PS3_Davila.sql
+++ ProblemSets/PS3/PS3_Davila.sql
@@ -1,12 +1,31 @@
-mode csv
+CREATE TABLE florida_insurance (
+    policyID INT,
+    statecode TEXT,
+    county TEXT,
+    eq_site_limit REAL,
+    hu_site_limit REAL,
+    fl_site_limit REAL,
+    fr_site_limit REAL,
+    tiv_2011 REAL,
+    tiv_2012 REAL,
+    eq_site_deductible REAL,
+    hu_site_deductible REAL,
+    fl_site_deductible REAL,
+    fr_site_deductible REAL,
+    point_latitude REAL,
+    point_longitude REAL,
+    line TEXT,
+    construction TEXT,
+    point_granularity INT
+);
 
-import /home/ouecon003/DScourseS23/ProblemSets/PS3/FL_insurance_sample.csv mydata; --read in data
+.mode csv
+.import /home/ouecon003/DScourseS23/ProblemSets/PS3/FL_insurance_sample.csv florida_insurance
 
-SELECT * FROM mydata LIMIT 10; --show first ten rows
+SELECT * FROM florida_insurance LIMIT 10;
 
-SELECT DISTINCT county FROM mydata; --select unique values of country variable
+SELECT DISTINCT county FROM florida_insurance;
 
-SELECT AVG(tiv_2012 - tiv_2011) AS avg_appreciation FROM mydata; --compute the average property appreciation from 2011 to 2012
+SELECT AVG(tiv_2012 - tiv_2011) AS avg_appreciation FROM florida_insurance;
 
-SELECT construction, COUNT(*) AS frequency FROM mydata GROUP BY construction; -- create frequency table by construction variable
- 
+SELECT construction, COUNT(*) AS frequency FROM florida_insurance GROUP BY construction; 
